ee9c53060416b03e193251be4a7359721da06d3f
Bryan <bryananta@icloud.com>
2025-06-01 01:15:46 +0700

    Update rename.py

--- tools/rename.py
+++ tools/rename.py
@@ -1,23 +1,23 @@
-#!/usr/bin/env python3
-
+import os
 import zipfile
 import plistlib
-import os
 
-ipa_path = "package/patched.ipa"
+patched_ipa = "package/patched.ipa"
+if not os.path.exists(patched_ipa):
+    raise FileNotFoundError("patched.ipa not found.")
 
-with zipfile.ZipFile(ipa_path, "r") as ipa:
-    plist_path = next(x for x in ipa.namelist() if x.endswith("Info.plist") and "Payload/" in x)
+with zipfile.ZipFile(patched_ipa) as ipa:
+    plist_path = next(x for x in ipa.namelist() if x.endswith("Info.plist") and x.startswith("Payload/"))
     with ipa.open(plist_path) as f:
         plist = plistlib.load(f)
-        name = plist.get("CFBundleDisplayName") or plist.get("CFBundleName")
-        version = plist.get("CFBundleShortVersionString")
+        name = plist.get("CFBundleDisplayName") or plist.get("CFBundleName") or "App"
+        version = plist.get("CFBundleShortVersionString") or "0.0"
 
 new_name = f"{name}_v{version}.ipa".replace(" ", "_")
 new_path = f"package/{new_name}"
 
-os.rename(ipa_path, new_path)
+os.rename(patched_ipa, new_path)
 
-# Simpan nama file untuk release
-with open("output_filename.txt", "w") as f:
-    f.write(new_path)
+# Output ke GitHub Actions
+with open(os.environ['GITHUB_OUTPUT'], "a") as out:
+    out.write(f"final_ipa_path={new_path}\n")
