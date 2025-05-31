c7ebb6665cf21053a064208784a8c78155d8a252
Bryan <bryananta@icloud.com>
2025-06-01 01:32:19 +0700

    Update rename.py

--- tools/rename.py
+++ tools/rename.py
@@ -13,11 +13,13 @@ with zipfile.ZipFile(patched_ipa) as ipa:
         name = plist.get("CFBundleDisplayName") or plist.get("CFBundleName") or "App"
         version = plist.get("CFBundleShortVersionString") or "0.0"
 
+# Create new name and sanitize spaces
 new_name = f"{name}_v{version}.ipa".replace(" ", "_")
 new_path = f"package/{new_name}"
 
+# Rename the IPA file
 os.rename(patched_ipa, new_path)
 
-# Output ke GitHub Actions
+# Output the final IPA path for GitHub Actions
 with open(os.environ['GITHUB_OUTPUT'], "a") as out:
     out.write(f"final_ipa_path={new_path}\n")
