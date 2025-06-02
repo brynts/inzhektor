4a125c8a35ef33f0bec6430e3d4fc8b3441532cd
Bryan <bryananta@icloud.com>
2025-06-02 09:39:33 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import subprocess
 
-# Ensure the correct path for ipapatch (assuming it's in the current directory)
 subprocess.run([
     "./ipapatch", "--noconfirm",
     "--input", "package/input.ipa",
